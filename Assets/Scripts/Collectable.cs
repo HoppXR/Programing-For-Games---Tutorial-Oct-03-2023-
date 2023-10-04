bc206e4ce8f717fa03a8d14baacd5f32b7c57026
HoppXR <jaden.chen1@ontariotechu.net>
2023-10-04 14:09:55 -0400

    Added Score Counter

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    public int Score = 0;
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
         {
             Destroy(gameObject);
+            Score++;
+            Debug.Log("Score: " + Score);
         }
     }
 }
